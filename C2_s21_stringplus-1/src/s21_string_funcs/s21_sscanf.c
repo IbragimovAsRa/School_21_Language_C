508ef169d8274b0715329192e40b6a645a5dcd96
Aslan Ibragimov <aslan18asker@gmail.com>
2023-02-15 15:14:06 +0300

    update 2.1

--- C2_s21_stringplus-1/src/s21_string_funcs/s21_sscanf.c
+++ C2_s21_stringplus-1/src/s21_string_funcs/s21_sscanf.c
@@ -4,18 +4,30 @@
 
 // символ с - пример - sscanf("ab")
 
-int s21_sscanf(const char *str, const char *format, ...)  {
+int s21_sscanf(const char *str, const char *format, ...) {
     va_list factor;
+    bool separator = false;
     va_start(factor, format);
     const char *sym_form_current = format;
     const char *sym_str_current = str;
     char *tmp_c;
 
-    while (*sym_form_current != '\0') { // brute force of format characters
-        if (*sym_form_current == '%') { // processing the specifier
+    while (*sym_form_current != '\0' && *sym_str_current != '\0') { // brute force of format characters
+        if (*sym_form_current == ' ') {
+            separator = true;
             sym_form_current = (sym_form_current + 1);
-            if (*sym_form_current == 'c') {
-                tmp_c = va_arg(factor, char*);
+        } else {
+            separator = false;
+        }
+        if (*sym_form_current == '%' && *(sym_form_current + 1) == 'c') { // processing the specifier
+            sym_form_current = (sym_form_current + 1);
+            tmp_c = va_arg(factor, char*);
+            if (separator) { // переместить курсор строки до первого символа не пробел
+                while (*sym_str_current == ' ' ) {
+                    sym_str_current += 1;
+                }
+            }
+            if (*sym_str_current != '\0') {
                 *tmp_c = *sym_str_current;
                 sym_str_current = sym_str_current + 1;
             }
@@ -30,5 +42,6 @@ int s21_sscanf(const char *str, const char *format, ...)  {
 // ФЛАГ %C (возможные исходы)
 // 1) sscanf("ab", "%c%c", &ch_1, &ch_2) - классический случай
 // 2) sscanf("a b", "%c%c", &ch_1, &ch_2) - нюансы с пробелами
+// 3) sscanf("a b", "     %c %c", &ch_1, &ch_2) - нюансы с пробелами
 
-
+// Документация (sscanf)
--- C2_s21_stringplus-1/src/test_cases/case_s21_sscanf.c
+++ C2_s21_stringplus-1/src/test_cases/case_s21_sscanf.c
@@ -22,12 +22,60 @@ START_TEST(sscanf_test_1) {
 
 END_TEST
 
+
+
+
+START_TEST(sscanf_test_2) {
+        char ch_1;
+        char ch_2;
+        char ch_3;
+
+        char s21_ch_1;
+        char s21_ch_2;
+        char s21_ch_3;
+
+
+        s21_sscanf("a bc", "%c%c%c", &s21_ch_1, &s21_ch_2, &s21_ch_3);
+        sscanf("a bc", "%c%c%c", &ch_1, &ch_2, &ch_3);
+
+        ck_assert_int_eq((int)s21_ch_1, (int) ch_1 );
+        ck_assert_int_eq((int)s21_ch_2, (int) ch_2 );
+        ck_assert_int_eq((int)s21_ch_3, (int) ch_3 );
+
+
+}
+
+END_TEST
+
+
+START_TEST(sscanf_test_3) {
+    char ch_1;
+    char ch_2;
+    char ch_3;
+
+    char s21_ch_1;
+    char s21_ch_2;
+    char s21_ch_3;
+    s21_sscanf("a bc", "%c %c%c", &s21_ch_1, &s21_ch_2, &s21_ch_3);
+    sscanf("a bc", "%c %c%c", &ch_1, &ch_2, &ch_3);
+
+    ck_assert_int_eq((int)s21_ch_1, (int) ch_1 );
+    ck_assert_int_eq((int)s21_ch_2, (int) ch_2 );
+    ck_assert_int_eq((int)s21_ch_3, (int) ch_3 );
+}
+END_TEST
+
+
 Suite *get_suite_s21_sscanf() {
     Suite *s = suite_create("s21_sscanf suite ");
     TCase *tc = tcase_create("s21_sscanf test case");
     suite_add_tcase(s, tc);
 
     tcase_add_test(tc, sscanf_test_1);
+    tcase_add_test(tc, sscanf_test_2);
+    tcase_add_test(tc, sscanf_test_3);
+
+
 
 
     return s;
