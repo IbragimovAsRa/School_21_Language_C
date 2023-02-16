18329bc87017397b790e0a92e284b37c08410562
Aslan Ibragimov <aslan18asker@gmail.com>
2023-02-16 22:25:34 +0300

    update 2.2

--- C2_s21_stringplus-1/src/s21_string_funcs/s21_sscanf.c
+++ C2_s21_stringplus-1/src/s21_string_funcs/s21_sscanf.c
@@ -1,47 +1,140 @@
 
 #include "../s21_string.h"
-
+#include <string.h>
 
 // символ с - пример - sscanf("ab")
+int get_length_pattern(const char *str);
+void filling_with_zeros(char *array, int array_size);
+bool check_separator(const char *ch);
+void carriage_leveler(const char **str);
+char check_specifier(const char *sym_form_current);
+void specifier_c_handler(const char **sym_form_current, const char **sym_str_current, bool separator_form, char *tmp_c);
+void specifier_d_handler();
+
 
 int s21_sscanf(const char *str, const char *format, ...) {
     va_list factor;
-    bool separator = false;
+    bool separator_form = false;
+    bool separator_str = false;
+
     va_start(factor, format);
     const char *sym_form_current = format;
     const char *sym_str_current = str;
     char *tmp_c;
+    int *tmp_d;
+    char tmp_str[100];
+    int length_pattern;
+    filling_with_zeros(tmp_str, 100);
+
+    //int read_char = 0;
 
     while (*sym_form_current != '\0' && *sym_str_current != '\0') { // brute force of format characters
-        if (*sym_form_current == ' ') {
-            separator = true;
-            sym_form_current = (sym_form_current + 1);
-        } else {
-            separator = false;
+
+        separator_form = check_separator(sym_form_current);
+        separator_str = check_separator(sym_str_current);
+
+        if (separator_form && separator_str) { // выровнять каретки
+            carriage_leveler(&sym_str_current);
+            carriage_leveler(&sym_form_current);
         }
-        if (*sym_form_current == '%' && *(sym_form_current + 1) == 'c') { // processing the specifier
-            sym_form_current = (sym_form_current + 1);
-            tmp_c = va_arg(factor, char*);
-            if (separator) { // переместить курсор строки до первого символа не пробел
-                while (*sym_str_current == ' ' ) {
-                    sym_str_current += 1;
+
+        switch (check_specifier(sym_form_current)) {
+            case 'c':
+                tmp_c = va_arg(factor, char*);
+                specifier_c_handler(&sym_form_current, &sym_str_current, separator_form, tmp_c);
+                break;
+            case 'd':
+                carriage_leveler(&sym_str_current);
+                sym_form_current = (sym_form_current + 1);
+                tmp_d = va_arg(factor, int*);
+                if (separator_form) { // переместить курсор строки до первого символа не пробел
+                    while (*sym_str_current == ' ' ) {
+                        sym_str_current += 1;
+                    }
                 }
-            }
-            if (*sym_str_current != '\0') {
-                *tmp_c = *sym_str_current;
-                sym_str_current = sym_str_current + 1;
-            }
+                if (*sym_str_current != '\0') {
+                    length_pattern = get_length_pattern(sym_str_current);
+                    strncpy(tmp_str, sym_str_current, length_pattern);
+                    *tmp_d = atoi(tmp_str); // обработать на ошибки
+                    sym_str_current = sym_str_current + length_pattern;
+                    filling_with_zeros(tmp_str, 100);
+                }
+                break;
         }
-        sym_form_current = (sym_form_current + 1);
+
+
+
+        sym_form_current++;
+        sym_str_current++;
+
     }
     va_end(factor);
     return 0;
 }
 
+void specifier_c_handler(const char **sym_form_current, const char **sym_str_current, bool separator_form, char *tmp_c) {
+    *sym_form_current = (*sym_form_current + 1);
+    if (separator_form) { // переместить курсор строки до первого символа не пробел
+        carriage_leveler(sym_str_current);
+    }
+    if (**sym_str_current != '\0') {
+        *tmp_c = **sym_str_current;
+    }
+}
+
+char check_specifier(const char *sym_form_current) {
+    char specifier = '!';
+    if (*sym_form_current == '%' && *(sym_form_current + 1) == 'c') {
+        specifier = 'c';
+    }
+    if (*sym_form_current == '%' && *(sym_form_current + 1) == 'd') {
+        specifier = 'd';
+    }
+    return specifier;
+}
+
+
+
+void carriage_leveler(const char **str) {
+    while (**str == ' ') {
+        *str = *str + 1;
+    }
+}
+
+
+
+bool check_separator(const char *ch) {
+    bool result = false;
+    if (*ch == ' ') {
+        result = true;
+    }
+    return result;
+}
+
+int get_length_pattern(const char *str) {
+    int count = 0;
+    while (!(*str ==  '\0'  ||  *str == ' ')) {
+        str++;
+        count++;
+    }
+    return count;
+}
+void filling_with_zeros(char *array, int array_size) {
+    for (int i = 0; i < array_size; i++) {
+        array[i] = '\0';
+    }
+}
 
 // ФЛАГ %C (возможные исходы)
 // 1) sscanf("ab", "%c%c", &ch_1, &ch_2) - классический случай
 // 2) sscanf("a b", "%c%c", &ch_1, &ch_2) - нюансы с пробелами
 // 3) sscanf("a b", "     %c %c", &ch_1, &ch_2) - нюансы с пробелами
 
+
+// ФЛАГ %D (возможные исходы)
+// 1) sscanf("mu number 1222223", "my number %d", &in_1) - классический случай
+// 2) sscanf("1222223", "%d", &ch_1, &ch_2) - классический случай
+
+
 // Документация (sscanf)
+// все строится на разделителях
--- C2_s21_stringplus-1/src/test_cases/case_s21_sscanf.c
+++ C2_s21_stringplus-1/src/test_cases/case_s21_sscanf.c
@@ -1,6 +1,28 @@
 #include "../s21_string_tests.h"
 
 
+START_TEST(sscanf_test_0) {
+    char ch_1;
+    char ch_2;
+    char ch_3;
+
+    char s21_ch_1;
+    char s21_ch_2;
+    char s21_ch_3;
+
+
+    s21_sscanf("my number abc", "my number %c%c%c", &s21_ch_1, &s21_ch_2, &s21_ch_3);
+    sscanf("my number abc", "my number %c%c%c", &ch_1, &ch_2, &ch_3);
+
+    ck_assert_int_eq((int)s21_ch_1, (int) ch_1 );
+    ck_assert_int_eq((int)s21_ch_2, (int) ch_2 );
+    ck_assert_int_eq((int)s21_ch_3, (int) ch_3 );
+
+}
+
+END_TEST
+
+
 START_TEST(sscanf_test_1) {
     char ch_1;
     char ch_2;
@@ -65,15 +87,66 @@ START_TEST(sscanf_test_3) {
 }
 END_TEST
 
+START_TEST(sscanf_test_4) {
+    int a;
+    int b;
+    s21_sscanf("228", "%d", &a);
+    sscanf("228", "%d", &b);
+
+    ck_assert_int_eq( a, b);
+
+}
+END_TEST
+
+START_TEST(sscanf_test_5) {
+    int a;
+    int b;
+    s21_sscanf("my number 228", "my number     %d", &a);
+    sscanf("my number 228", "my number     %d", &b);
+
+    ck_assert_int_eq( a, b);
+
+}
+END_TEST
+
+START_TEST(sscanf_test_6) {
+    int a;
+    int b;
+    s21_sscanf("my number 228", "my number %d", &a);
+    sscanf("my number 228", "my number %d", &b);
+
+    ck_assert_int_eq( a, b);
+
+}
+END_TEST
+
+START_TEST(sscanf_test_7) {
+    int a;
+    int b;
+    s21_sscanf("my number 228", "my number%d", &a);
+    sscanf("my number 228", "my number%d", &b);
+
+    ck_assert_int_eq( a, b);
+
+}
+END_TEST
 
 Suite *get_suite_s21_sscanf() {
     Suite *s = suite_create("s21_sscanf suite ");
     TCase *tc = tcase_create("s21_sscanf test case");
     suite_add_tcase(s, tc);
 
+    tcase_add_test(tc, sscanf_test_0);
     tcase_add_test(tc, sscanf_test_1);
     tcase_add_test(tc, sscanf_test_2);
     tcase_add_test(tc, sscanf_test_3);
+    tcase_add_test(tc, sscanf_test_4);
+    tcase_add_test(tc, sscanf_test_5);
+    tcase_add_test(tc, sscanf_test_6);
+    tcase_add_test(tc, sscanf_test_7);
+
+
+
 
 
 
